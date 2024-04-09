ba40277b199c2d1b69aa54b7caa923af3ccfbee6
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-04-09 20:27:52 -0300

    :construction: feat(main): Atualizacao do menu

--- src/br/com/projetoMvc/Main.java
+++ src/br/com/projetoMvc/Main.java
@@ -12,65 +12,69 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String menu = "*** Menu de opções ***"
-				.concat("\n[1] Listar todos")
-				.concat("\n[2] Listar por ID")
-				.concat("\n[3] Cadastrar")
-				.concat("\n[4] Alterar")
-				.concat("\n[5] Excluir")
-				.concat("\n[0] Sair");
+		ProdutoController controller = new ProdutoController();
+		int opcao = 0;
 		
-		int opcao = Integer.parseInt(JOptionPane.showInputDialog(menu));
-		
-		switch (opcao) {
-		case 1:
-			JOptionPane.showMessageDialog(null, "Listar todos");
-			break;
-		case 2:
-			JOptionPane.showMessageDialog(null, "Listar por ID");
-			break;
-		case 3:
-			JOptionPane.showMessageDialog(null, "Cadastrar");
-			break;
-		case 4:
-			JOptionPane.showMessageDialog(null, "Alterar");
-			break;
-		case 5:
-			JOptionPane.showMessageDialog(null, "Excluir");
-			break;
-		case 0:
-			JOptionPane.showMessageDialog(null, "Saindo do menu...");
-			break;
-		default:
-			JOptionPane.showMessageDialog(null, "Opção inválida pra o código " + opcao);
-		}
-		
-//		ProdutoController controller = new ProdutoController();
-//		
-//		Produto novoProduto = new Produto();
-//		novoProduto.setDescricao(JOptionPane.showInputDialog("Descrição do produto"));
-//		
-//		controller.cadastrar(novoProduto);
-//		
-//		
-//		List<Produto> lista = new ArrayList<Produto>();
-//		lista = controller.listarTodos();
-//		
-//		String mensagemLista = ""
-//				.concat("- Lista de produtos -")
-//				.concat("\n")
-//				.concat("Cód.   Descrição");
-//		
-//		for (Produto produto : lista) {
-//			mensagemLista = mensagemLista
-//					.concat("\n")
-//					.concat( String.valueOf(produto.getId()) )
-//					.concat("        ")
-//					.concat(produto.getDescricao());
-//		}
-//		
-//		JOptionPane.showMessageDialog(null, mensagemLista);
-		
+		do {
+			String menu = "*** Menu de opções ***"
+					.concat("\n[1] Listar todos")
+					.concat("\n[2] Listar por ID")
+					.concat("\n[3] Cadastrar")
+					.concat("\n[4] Alterar")
+					.concat("\n[5] Excluir")
+					.concat("\n[0] Sair");
+			
+			opcao = Integer.parseInt(JOptionPane.showInputDialog(menu));
+			
+			switch (opcao) {
+			case 1: // Listar todos
+				List<Produto> lista = new ArrayList<Produto>();
+				lista = controller.listarTodos();
+				
+				if (lista.size() > 0) {
+					
+					String mensagemLista = ""
+							.concat("- Lista de produtos -")
+							.concat("\n")
+							.concat("Cód.   Descrição");
+					
+					for (Produto produto : lista) {
+						mensagemLista = mensagemLista
+								.concat("\n")
+								.concat( String.valueOf(produto.getId()) )
+								.concat("        ")
+								.concat(produto.getDescricao());
+					}
+					
+					JOptionPane.showMessageDialog(null, mensagemLista);
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "Não existem produtos cadastrados!");
+				}
+				
+				break;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Listar por ID");
+				break;
+			case 3: // Cadastrar
+				Produto novoProduto = new Produto();
+				novoProduto.setDescricao(JOptionPane.showInputDialog("Descrição do produto"));
+				
+				controller.cadastrar(novoProduto);
+				break;
+			case 4:
+				JOptionPane.showMessageDialog(null, "Alterar");
+				break;
+			case 5:
+				JOptionPane.showMessageDialog(null, "Excluir");
+				break;
+			case 0:
+				JOptionPane.showMessageDialog(null, "Saindo do sistema...");
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Opção inválida pra o código " + opcao);
+			}
+		} while (opcao != 0);
 		
 
 	}
